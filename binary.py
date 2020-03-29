0af7c052c9823e321327c938ec8a669d8f615b06
Christopher Siu <christopher@siu.sh>
2020-03-29 12:25:50 -0700

    Initial commit.

---
+++ binary.py
@@ -0,0 +1,61 @@
+# Implements operations on binary numbers.
+# CSC 225, Assignment 1
+# Given code, Winter '20
+
+
+def add(addend_a, addend_b):
+    """
+    Add two 8-bit, two's complement binary numbers; ignore carries/overflows.
+    TODO: Implement this function. Do *not* convert the numbers to decimal.
+
+    :param addend_a: A bitstring representing the first number
+    :param addend_b: A bitstring representing the second number
+    :return: A bitstring representing the sum
+    """
+    pass
+
+
+def negate(number):
+    """
+    Negate an 8-bit, two's complement binary number.
+    TODO: Implement this function. Do *not* convert the number to decimal.
+
+    :param number: A bitstring representing the number to negate
+    :return: A bistring representing the negated number
+    """
+    pass
+
+
+def subtract(minuend, subtrahend):
+    """
+    Subtract one 8-bit, two's complement binary number from another.
+    TODO: Implement this function. Do *not* convert the numbers to decimal.
+
+    :param minuend: A bitstring representing the number from which to subtract
+    :param subtrahend: A bitstring representing the number to subtract
+    :return: A bitstring representing the difference
+    """
+    pass
+
+
+def binary_to_decimal(number):
+    """
+    Convert an 8-bit, two's complement binary number to decimal.
+    TODO: Implement this function.
+
+    :param number: A bitstring representing the number to convert
+    :return: An integer, the converted number
+    """
+    pass
+
+
+def decimal_to_binary(number):
+    """
+    Convert a decimal number to 8-bit, two's complement binary.
+    TODO: Implement this function.
+
+    :param number: An integer, the number to convert
+    :return: A bitstring representing the converted number
+    :raise OverflowError: If the number cannot be represented with 8 bits
+    """
+    pass
---
+++ hexadecimal.py
@@ -0,0 +1,25 @@
+# Implements operations on hexadecimal numbers.
+# CSC 225, Assignment 1
+# Given code, Winter '20
+
+
+def binary_to_hex(number):
+    """
+    Convert a 16-bit binary number to hexadecimal.
+    TODO: Implement this function.
+
+    :param number: A bitstring representing the number to convert
+    :return: A hexadecimal string, the converted number
+    """
+    pass
+
+
+def hex_to_binary(number):
+    """
+    Convert a hexadecimal number to 16-bit binary.
+    TODO: Implement this function.
+
+    :param number: A hexadecimal string, the number to convert
+    :return: A bitstring representing the converted number
+    """
+    pass
